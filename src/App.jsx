5eaa5410245eb6f4b712cd787b600f8a70e2e757
J <0xjariel@protonmail.com>
2023-08-12 01:42:37 -0600

    create Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { ReactDOM } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Profile from "./Profile";
 import "./App.css";
 
-const App = () => <h1>Our First Test</h1>;
+const App = () => (
+  <div>
+    <h1>Hello from the main page of the app!</h1>
+    <p>Here are some examples of links to other pahes</p>
+    <nav>
+      <ul>
+        <li>
+          <a href="profile">Profile page</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+);
 
 export default App;
